d680cd99a4afbd66a6fd908607ae304a98e0f194
Highway <milanimilivoj@gmail.com>
2021-09-12 09:58:20 +0200

    List and search members
    Signed-off-by: Highway <milanimilivoj@gmail.com>

--- roster/src/Roster.Core/Storage/IMemberStorage.cs
+++ roster/src/Roster.Core/Storage/IMemberStorage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Roster.Core.Domain;
 
@@ -6,5 +7,6 @@ namespace Roster.Core.Storage
     public interface IMemberStorage : IStorage<Member>
     {
         ICollection<string> GetAllNicknames();
+        PaginatedList<Member> Page(ISpecification<Member> member, Func<Member, object> orderKeySelector, int page, int pageSize);
     }
 }
