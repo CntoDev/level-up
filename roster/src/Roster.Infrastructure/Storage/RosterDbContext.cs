2a0217e06a8f48d513dae72735338d7864db2c66
Highway <milanimilivoj@gmail.com>
2021-10-31 14:52:50 +0100

    Fix EF for value objects in domain logic
    * Fix entity framework layer
    
    * Slight improvement of exception handling

--- roster/src/Roster.Infrastructure/Storage/RosterDbContext.cs
+++ roster/src/Roster.Infrastructure/Storage/RosterDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Roster.Core.Domain;
 
 namespace Roster.Infrastructure.Storage
@@ -16,11 +17,39 @@ namespace Roster.Infrastructure.Storage
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region MemberNickname
+
+            var memberNicknameConverter = new ValueConverter<MemberNickname, string>(
+                mn => mn.Nickname,
+                v => new MemberNickname(v, true)
+            );
+
+            #endregion
+
             modelBuilder.Entity<ApplicationForm>()
                         .HasKey(af => af.Nickname);
             
             modelBuilder.Entity<ApplicationForm>()
                         .OwnsMany<Arma3Dlc>(af => af.OwnedDlcs);
+
+            modelBuilder.Entity<ApplicationForm>()
+                        .OwnsOne<EmailAddress>(af => af.Email)
+                        .Property(a => a.Email)
+                        .HasColumnName("Email");
+
+            modelBuilder.Entity<ApplicationForm>()
+                        .OwnsOne<EmailAddress>(af => af.Gmail)
+                        .Property(a => a.Email)
+                        .HasColumnName("Gmail");
+
+            modelBuilder.Entity<ApplicationForm>()
+                        .Property(af => af.Nickname)
+                        .HasConversion(memberNicknameConverter);
+
+            modelBuilder.Entity<ApplicationForm>()
+                        .OwnsOne<DiscordId>(af => af.DiscordId)
+                        .Property(d => d.Id)
+                        .HasColumnName("DiscordId");
 
             modelBuilder.Entity<Member>()
                         .HasKey(m => m.Nickname);
