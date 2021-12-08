b7d2bc14714b80cff2f88f45db6a3884dd76fdfb
Highway <milanimilivoj@gmail.com>
2021-12-08 19:39:40 +0100

    Bump to 0.4.0

--- roster/Build-Docker.ps1
+++ roster/Build-Docker.ps1
@@ -5,9 +5,9 @@ if (Test-Path .\docker-build) {
 
 docker container stop cnto-roster
 docker container rm cnto-roster
-docker image rm cntoarma/roster:0.3.0-snapshot
+docker image rm cntoarma/roster:0.4.0-snapshot
 
 # Build new image
 dotnet publish .\src\Roster.Web --configuration Release --output .\docker-build
 Copy-Item .\container\Dockerfile .\docker-build
-docker build -t cntoarma/roster:0.3.0-snapshot .\docker-build
+docker build -t cntoarma/roster:0.4.0-snapshot .\docker-build
