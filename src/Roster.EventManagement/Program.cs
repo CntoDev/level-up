0f90fc4cee8f9cc52c536ad1cfdac1b2a05590e3
Highway <milanimilivoj@gmail.com>
2021-12-25 10:36:43 +0100

    Research getting players info from Arma 3 server #96

---
+++ src/Roster.EventManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using SteamQueryNet.Interfaces;
+using SteamQueryNet;
+using Newtonsoft.Json;
+
+string serverIp = "194.68.41.7";
+ushort serverPort = 2303;
+
+IServerQuery serverQuery = new ServerQuery(serverIp, serverPort);
+var players = serverQuery.GetPlayers();
+
+foreach (var player in players)
+{
+    string json = JsonConvert.SerializeObject(player);
+    Console.WriteLine(json);
+}
