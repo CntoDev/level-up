14a2b282056977b898cbba9fed5b4857c867eae9
Highway <milanimilivoj@gmail.com>
2022-01-18 00:05:00 +0100

    Data seeding #104

---
+++ data/seed.sql
@@ -0,0 +1,66 @@
+START TRANSACTION;
+
+insert into "Dlcs"
+values ('Karts');
+
+insert into "Dlcs"
+values ('Helicopters');
+
+insert into "Dlcs"
+values ('Marksmen');
+
+insert into "Dlcs"
+values ('Apex');
+
+insert into "Dlcs"
+values ('Jets');
+
+insert into "Dlcs"
+values ('Malden');
+
+insert into "Dlcs"
+values ('Laws of War');
+
+insert into "Dlcs"
+values ('Tac-Ops');
+
+insert into "Dlcs"
+values ('Tanks');
+
+insert into "Dlcs"
+values ('Contact');
+
+insert into "Dlcs"
+values ('Art of War');
+
+insert into "Dlcs"
+values ('CSLA: Iron Curtain');
+
+insert into "Dlcs"
+values ('CSLA: Global Mobilization');
+
+insert into "Dlcs"
+values ('S.O.G. Prairie Fire');
+
+insert into "Dlcs"
+values ('Western Sahara');
+
+insert into "Ranks"
+values (1, 'Recruit');
+
+insert into "Ranks"
+values (2, 'Grunt');
+
+insert into "Ranks"
+values (3, 'Reservist');
+
+insert into "Ranks"
+values (4, 'Specialist');
+
+insert into "Ranks"
+values (5, 'Corporal');
+
+insert into "Ranks"
+values (6, 'Staff Sergeant');
+
+commit;
