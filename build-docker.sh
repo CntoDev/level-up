22d5dbc4a435b7f5c3bdcebda89b3b918db36658
Highway <milanimilivoj@gmail.com>
2022-01-19 16:23:05 +0100

    Dockerize Discord service #106

--- build-docker.sh
+++ build-docker.sh
@@ -14,10 +14,28 @@ if [[ -n "$CNTO_ROSTER_CONTAINER" ]]; then
 	echo "Stopping and removing cnto-arma container..."
 	sudo docker container stop cnto-arma; \
 	docker container rm cnto-arma; \
-	docker image rm cnto-arma:$CNTO_ROSTER_VERSION
+	docker image rm cntoarma/rooster:$CNTO_ROSTER_VERSION
+
+	echo "Stopping and removing cnto-arma-discord container"
+	sudo docker container stop cnto-arma-discord; \
+	docker container rm cnto-arma-discord; \
+	docker image rm cntoarma/rooster-discord:$CNTO_ROSTER_VERSION
 fi
 
-# new build
+# build core service
+echo "Building Rooster core service..."
 dotnet publish src/Roster.Web -c Release -o docker-build
-cp container/Dockerfile docker-build/Dockerfile
-sudo docker build -t cntoarma/roster:$CNTO_ROSTER_VERSION ./docker-build
+sudo docker build -f container/Dockerfile -t cntoarma/rooster:$CNTO_ROSTER_VERSION ./docker-build
+echo "Rooster core service built."
+
+# clear build folder
+rm -rf docker-build/*
+
+# build discord service
+echo "Building Rooster Discord service..."
+dotnet publish src/Roster.DiscordService -c Release -o docker-build
+sudo docker build -f container/Dockerfile-DiscordService -t cntoarma/rooster-discord:$CNTO_ROSTER_VERSION ./docker-build
+echo "Rooster Discord service built."
+
+# tidy after build
+rm -rf docker-build
