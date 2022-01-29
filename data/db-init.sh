fec15bd5bc86cb240520e9db3c3bfa213418dfef
Highway <milanimilivoj@gmail.com>
2022-01-29 16:04:59 +0100

    Add pre-built dockerized Rooster Postgres database with seeded data

---
+++ data/db-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file /data/build-db.sql
+psql --username "$POSTGRES_USER" --dbname identity --file /data/identity.sql
+psql --username "$POSTGRES_USER" --dbname roster --file /data/roster.sql
+psql --username "$POSTGRES_USER" --dbname roster --file /data/process.sql
+psql --username "$POSTGRES_USER" --dbname roster --file /data/seed.sql
---
+++ data/process.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE "RecruitmentSaga" (
+    "CorrelationId" uuid NOT NULL,
+    "Nickname" text NULL,
+    "AutomaticDischarge" boolean NOT NULL,
+    "RecruitmentStartDate" timestamp with time zone NULL,
+    "ModsCheckDate" timestamp with time zone NULL,
+    "BootcampCompletionDate" timestamp with time zone NULL,
+    "EnoughAttendedEvents" boolean NOT NULL,
+    "TrialSucceeded" boolean NULL,
+    CONSTRAINT "PK_RecruitmentSaga" PRIMARY KEY ("CorrelationId")
+);
+
+
