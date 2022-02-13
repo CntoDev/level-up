ada1bf4a226f42e1b196e4ac2c376eeaae2cfa3e
Highway <milanimilivoj@gmail.com>
2022-02-13 10:46:15 +0100

    Example of how to attach to Old Roster

---
+++ src/Roster.Old.Flood/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+Console.WriteLine("Starting Old Roster Flood...");
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+var conn = new NpgsqlConnection(configuration.GetConnectionString("OldRoster"));
+conn.Open();
+
+conn.Notification += (o, e) => { 
+    Console.WriteLine($"Received event {e.Payload}."); 
+};
+
+using (var cmd = new NpgsqlCommand("LISTEN members_stream", conn))
+{
+    cmd.ExecuteNonQuery();
+}
+
+while(true)
+{
+    conn.Wait();
+}
